3fb3c7ee2a3fce82b8ec22d1e8fc3fbe2ab21666
Tryndo225 <147951467+Tryndo225@users.noreply.github.com>
2024-09-12 14:44:42 +0200

    Update Booster.cs

--- Assets/TIles/Scripts/Booster.cs
+++ Assets/TIles/Scripts/Booster.cs
@@ -15,13 +15,33 @@ public class Booster : MonoBehaviour
     // Audio clip file
     [SerializeField] private AudioClip soundEffect;
 
+    // Animator purposes
+    private int time;
+
     // Runns when establishing collision with the player speeding the player up by a factor 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            this.gameObject.GetComponent<Animator>().SetBool("Used", true);
+            time = 1;
             playerBody.velocity = new Vector2(playerBody.velocity.x * boostStrenghtHorizontal,  playerBody.velocity.y * boostStrenghtVertical);
             SoundFXManager.soundFXManagerInstance.playSoundFXClip(soundEffect, this.transform);
+        }
+    }
+
+    // For animator purposes
+    private void Update()
+    {
+        if (time < 2 && time != 0)
+        {
+            time++;
+        }
+
+        else if (time >= 2)
+        {
+            time = 0;
+            this.gameObject.GetComponent<Animator>().SetBool("Used", false);
         }
     }
 }
